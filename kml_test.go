6407f4401eee7ccec7d75f4862f00b0ad0f8d331
Tom Payne <twpayne@gmail.com>
2015-11-22 20:17:32 +0100

    Use WriteIndent in example

--- kml_test.go
+++ kml_test.go
@@ -46,9 +46,20 @@ func ExampleKML() {
 			),
 		),
 	)
-	if err := k.Write(os.Stdout); err != nil {
+	if err := k.WriteIndent(os.Stdout, "", "  "); err != nil {
 		log.Fatal(err)
 	}
+	// Output:
+	// <?xml version="1.0" encoding="UTF-8"?>
+	// <kml xmlns="http://www.opengis.net/kml/2.2">
+	//   <Placemark>
+	//     <name>Simple placemark</name>
+	//     <description>Attached to the ground. Intelligently places itself at the height of the underlying terrain.</description>
+	//     <Point>
+	//       <coordinates>-122.0822035425683,37.42228990140251</coordinates>
+	//     </Point>
+	//   </Placemark>
+	// </kml>
 }
 
 func TestSimpleElements(t *testing.T) {
